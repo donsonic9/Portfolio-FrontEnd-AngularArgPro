9da3d62b876d50ab7baa350503a8a87c629718bb
donsonic9 <donsonic9@gmail.com>
2023-05-14 19:27:28 -0300

    WorkingGoogleLogIn

--- src/app/components/user-inicio/user-inicio.component.ts
+++ src/app/components/user-inicio/user-inicio.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Mensaje } from 'src/app/models/mensaje';
+import { AuthService } from 'src/app/services/auth.service';
+import { DataPortfolioService } from 'src/app/services/data-portfolio.service';
 
 @Component({
   selector: 'app-user-inicio',
   templateUrl: './user-inicio.component.html',
   styleUrls: ['./user-inicio.component.css']
 })
-export class UserInicioComponent {
+export class UserInicioComponent implements OnInit{
+
+  miPortfolio: Mensaje[] = [];
+
+  constructor(private datosPortfolio: DataPortfolioService,
+    private auth: AuthService) {
+
+  }
+
+  ngOnInit(): void {
+    // const date = new Date().toLocaleDateString('en-US', {
+    //   day: '2-digit',
+    //   month: 'long',
+    //   year: 'numeric'
+    // });
+    const date = new Date().toLocaleString('en-US', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      timeZoneName: 'short'
+    });
+    
+    const userGoogle = this.auth.getUserLogged();
+    const username = userGoogle?.displayName || '';
+    const email = userGoogle?.email || '';
+    const img_photo = userGoogle?.photoURL || '';
+
+    const men: Mensaje = {
+      fecha: date,
+      username: username,
+      email: email,
+      img_photo: img_photo,
+    };
+
+    // console.log(men);
+
+    this.datosPortfolio.crearMensaje(men).subscribe(() => {
+      
+    });
+  }
 
 }
